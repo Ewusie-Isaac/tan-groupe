84c352a4f7db7483019339589f975cda661b24c8
Ewusie-Isaac <owusu.lds@gmail.com>
2024-06-18 15:59:03 +0000

    valkem nav component updated

--- components/valkem/Navigation.tsx
+++ components/valkem/Navigation.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import Action from '../Action';
+
 // import { useSelectedLayoutSegment } from 'next/navigation';
 
 const Navigation = () => {
